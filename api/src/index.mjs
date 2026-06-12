61e49ecb26442e38b58766e5cd43a98dbf957bd4
Shipra Srivastava <shiprasrvstv1988@gmail.com>
2026-06-12 20:46:37 +0200

    chore: add health check endpoint

--- api/src/index.mjs
+++ api/src/index.mjs
@@ -1,13 +1,12 @@
 import "dotenv/config";
 import express from "express";
 import cors from "cors";
-import bodyParser from "body-parser";
 import knex from "./database_client.js";
 import nestedRouter from "./routers/nested.js";
 
 const app = express();
 app.use(cors());
-app.use(bodyParser.json());
+app.use(express.json());
 
 const apiRouter = express.Router();
 
@@ -28,6 +27,14 @@ apiRouter.get("/", async (req, res) => {
 apiRouter.use("/nested", nestedRouter);
 
 app.use("/api", apiRouter);
+
+app.get("/api/health", (req, res) => {
+  res.status(200).json({
+    success: true,
+    message: "ReadTogether backend server is running and healthy!",
+    timestamp: new Date(),
+  });
+});
 
 app.listen(PORT, () => {
   console.log(`API listening on port ${PORT}`);
