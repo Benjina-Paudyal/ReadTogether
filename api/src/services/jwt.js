925bac03ef75e05bbcda2042841a74e696e6a9ad
jamshed <32561969+jamshed5@users.noreply.github.com>
2026-06-21 15:46:17 +0200

    conver services from object to module (#15)

--- api/src/services/jwt.js
+++ api/src/services/jwt.js
@@ -3,14 +3,13 @@ import jwt from "jsonwebtoken";
 const JWT_SECRET = process.env.JWT_SECRET;
 const JWT_EXPIRES_IN = process.env.JWT_EXPIRES_IN || "1d";
 
-export const JwtService = {
-  issueToken(payload) {
-    return jwt.sign(payload, JWT_SECRET, {
-      expiresIn: JWT_EXPIRES_IN,
-    });
-  },
 
-  verifyToken(token) {
-    return jwt.verify(token, JWT_SECRET);
-  },
+export function issueToken(payload) {
+  return jwt.sign(payload, JWT_SECRET, {
+    expiresIn: JWT_EXPIRES_IN,
+  });
+};
+
+export function verifyToken(token) {
+  return jwt.verify(token, JWT_SECRET);
 };
