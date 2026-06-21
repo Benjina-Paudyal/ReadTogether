8296890e2d57d1fc0997fb44ef37a21699d82751
jamshed <jaasol.ali@gmail.com>
2026-06-21 14:57:28 +0200

    added encription service (independent)

---
+++ api/src/configs/swagger.js
@@ -0,0 +1,53 @@
+import swaggerJSDoc from "swagger-jsdoc";
+import "dotenv/config";
+
+const PORT = process.env.PORT || 3000;
+
+const options = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "Books API",
+      version: "1.0.0",
+      description: "Simple MVC Books API"
+    },
+
+    
+    tags: [
+      {
+        name: "Auth",
+        description: "Authentication APIs"
+      },
+      {
+        name: "User",
+        description: "User APIs"
+      },
+      
+    ],
+
+    servers: [
+      {
+        url: `http://localhost:${PORT}/api`
+      }
+    ],
+
+    // ADD THIS (JWT AUTH)
+    components: {
+      securitySchemes: {
+        bearerAuth: {
+          type: "http",
+          scheme: "bearer",
+          bearerFormat: "JWT"
+        }
+      }
+    },
+
+
+  },
+
+  apis: ["./src/routers/*.js"]
+};
+
+const swaggerSpec = swaggerJSDoc(options);
+
+export default swaggerSpec;
